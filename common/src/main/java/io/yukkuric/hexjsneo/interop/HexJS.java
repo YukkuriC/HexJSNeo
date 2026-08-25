107e2107ae086a39af5f9d7973d1711e5c4392dc
YukkuriC <799433638@qq.com>
2026-08-26 00:02:00 +0800

    real nested registry

--- common/src/main/java/io/yukkuric/hexjsneo/interop/HexJS.java
+++
@@ -1,9 +0,0 @@
-package io.yukkuric.hexjsneo.interop;
-
-import io.yukkuric.hexjsneo.casting.ActionJS;
-import io.yukkuric.hexjsneo.casting.ActionRegistryJS;
-
-public interface HexJS {
-    Object ActionJS = ActionJS.class;
-    Object ActionRegistryJS = ActionRegistryJS.class;
-}
--- common/src/main/java/io/yukkuric/hexjsneo/interop/KJSPluginHJS.java
+++ common/src/main/java/io/yukkuric/hexjsneo/interop/KJSPluginHJS.java
@@ -4,12 +4,21 @@ import at.petrak.hexcasting.api.casting.eval.sideeffects.OperatorSideEffect;
 import at.petrak.hexcasting.api.casting.math.HexPattern;
 import dev.latvian.mods.kubejs.plugin.KubeJSPlugin;
 import dev.latvian.mods.kubejs.script.BindingRegistry;
+import dev.latvian.mods.rhino.NativeObject;
+import io.yukkuric.hexjsneo.casting.ActionJS;
+import io.yukkuric.hexjsneo.casting.ActionRegistryJS;
 
 public class KJSPluginHJS implements KubeJSPlugin {
     public void registerBindings(BindingRegistry event) {
         if (event.type().isClient()) return;
-        event.add("HexJS", HexJS.class);
         event.add("HexPattern", HexPattern.class);
         event.add("OperatorSideEffect", OperatorSideEffect.class);
+
+        // build HexJS object
+        var context = event.context();
+        var HexJS = new NativeObject(context.factory);
+        context.addToScope(HexJS, "ActionJS", ActionJS.class);
+        context.addToScope(HexJS, "ActionRegistryJS", ActionRegistryJS.class);
+        event.add("HexJS", HexJS);
     }
 }
