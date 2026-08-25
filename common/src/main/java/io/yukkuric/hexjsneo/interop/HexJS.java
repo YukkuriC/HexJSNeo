2b70ff0aa617772bc0ef5b802feee4207f811302
YukkuriC <799433638@qq.com>
2026-08-25 23:11:37 +0800

    add bindings
    adjust bindings

---
+++ common/src/main/java/io/yukkuric/hexjsneo/interop/HexJS.java
@@ -0,0 +1,9 @@
+package io.yukkuric.hexjsneo.interop;
+
+import io.yukkuric.hexjsneo.casting.ActionJS;
+import io.yukkuric.hexjsneo.casting.ActionRegistryJS;
+
+public interface HexJS {
+    Object ActionJS = ActionJS.class;
+    Object ActionRegistryJS = ActionRegistryJS.class;
+}
--- common/src/main/java/io/yukkuric/hexjsneo/interop/KJSPluginHJS.java
+++ common/src/main/java/io/yukkuric/hexjsneo/interop/KJSPluginHJS.java
@@ -1,10 +1,15 @@
 package io.yukkuric.hexjsneo.interop;
 
+import at.petrak.hexcasting.api.casting.eval.sideeffects.OperatorSideEffect;
+import at.petrak.hexcasting.api.casting.math.HexPattern;
 import dev.latvian.mods.kubejs.plugin.KubeJSPlugin;
 import dev.latvian.mods.kubejs.script.BindingRegistry;
 
 public class KJSPluginHJS implements KubeJSPlugin {
     public void registerBindings(BindingRegistry event) {
         if (event.type().isClient()) return;
+        event.add("HexJS", HexJS.class);
+        event.add("HexPattern", HexPattern.class);
+        event.add("OperatorSideEffect", OperatorSideEffect.class);
     }
 }
